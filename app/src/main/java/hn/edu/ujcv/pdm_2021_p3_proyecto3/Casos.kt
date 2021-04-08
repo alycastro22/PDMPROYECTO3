4710a0218d3123bbd4c80de2780798165547d903
alycastro22 <allisson.castro@ujcv.edu.hn>
2021-04-07 22:16:20 -0600

    Login,2 pantallas

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/Casos.kt
@@ -0,0 +1,5 @@
+package hn.edu.ujcv.pdm_2021_p3_proyecto3
+
+class Casos
+{
+}
---
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/MainActivity.kt
@@ -0,0 +1,16 @@
+package hn.edu.ujcv.pdm_2021_p3_proyecto3
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+    }
