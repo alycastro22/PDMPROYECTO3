1d31702ea3b69148ffe2fa095c00c95075b18200
aramel08 <78820331+aramel08@users.noreply.github.com>
2021-04-14 17:44:07 -0600

    ingresar

--- app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/LoginActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/LoginActivity.kt
@@ -1,11 +1,21 @@
 package hn.edu.ujcv.pdm_2021_p3_proyecto3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        txvRegistrar.setOnClickListener { ingresar() }
+    }
+
+    fun ingresar(){
+        val intent = Intent(this,MenuActivity::class.java)
+        startActivity(intent)
+
     }
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/MainActivity.kt
@@ -1,20 +1,23 @@
 package hn.edu.ujcv.pdm_2021_p3_proyecto3
 
+import android.content.Intent
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
+        txvCambiar.setOnClickListener { ingresar() }
 
     }
 
-
+    fun ingresar(){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
 
     }
+}
