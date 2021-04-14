122cdd50995ef972dee922b3b6935feee1d42991
alycastro22 <allisson.castro@ujcv.edu.hn>
2021-04-14 15:34:56 -0600

    Codigo Menu

--- app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/MenuActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/MenuActivity.kt
@@ -1,11 +1,24 @@
 package hn.edu.ujcv.pdm_2021_p3_proyecto3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity: AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        val card1: View = findViewById(R.id.card_servicios)
+        card1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.card_servicios -> {
+                val intent = Intent(this,ServiciosActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
