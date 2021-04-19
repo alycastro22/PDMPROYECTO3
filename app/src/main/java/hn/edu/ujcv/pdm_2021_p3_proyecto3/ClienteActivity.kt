e83ea76668f1c900ad0d8125f9b2e714c02ed39e
nahomi <nahomi.martinez@ujcv.edu.hn>
2021-04-18 20:22:46 -0600

    Holis

--- app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/ClienteActivity.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_p3_proyecto3/ClienteActivity.kt
@@ -140,32 +140,23 @@ class ClienteActivity : AppCompatActivity() {
             }
         })
     }
-    private fun obtenertexto(){
-        var Id = txtId12.text.toString().toLong()
-        var fecha = txtFechaNacimiento2.text.toString()
-        var nombre= txtNombre10.text.toString()
-        var correo=txtCorreo3.text.toString()
-        var telefono = txtTelefono2.text.toString().toLong()
-        println("Id:" + Id.toString() +  "fecha:" + fecha.toString() + "nombre:" + nombre.toString() +
-                 "correo"+ correo + "telefono:" + telefono.toString() )
-    }
+
 
     private fun callServicePostPerson() {
-       /* var Id = txtId.text.toString()
-        var fecha = txtFechaNaciAbo.text.toString()
-        var nombre=txtNombre1.text.toString()
-        var dni = txtIdentidad.text.toString()
-        var correo=txtCorreo1.text.toString()
-        var telefono = txtTelefono.text.toString().toInt()*/
-
-
+        var Id = txtId12.text.toString().toLong()
+        var fecha = txtFechaNacimiento2.text.toString()
+        var nombre = txtNombre10.text.toString()
+        var correo = txtCorreo3.text.toString()
+        var telefono = txtTelefono2.text.toString().toLong()
+        println("Id:" + Id.toString() +  "fecha:" + fecha + "nombre:" + nombre + "correo"+ correo + "telefono:" + telefono.toString() )
         //val fecha = "1995-12-06"
-        val personInfo = ClienteDataCollectionItem(  id = null,
 
-            nombre = "nombre",
-            correo= "correo",
-            fechaNacimiento = "fecha",
-            telefono = 1234
+        val personInfo = ClienteDataCollectionItem(  id = null,
+                nombre = nombre,
+                correo= correo,
+                fechaNacimiento = fecha,
+                telefono = telefono
+
         )
 
 
